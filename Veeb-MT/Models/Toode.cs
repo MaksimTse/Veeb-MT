accb8f521bcb6c9665e41d1afa87b8f241b6af5b
opilane <opilane@HP-CZC2349HTF>
2024-10-08 13:31:44 +0300

    Add project files.

---
+++ Veeb-MT/Models/Toode.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Veeb_MT.Models
+{
+    public class Toode
+    {
+        public int Id { get; set; }
+        public string Nimi { get; set; }
+        public double Price { get; set; }
+        public bool Aktiivne { get; set; }
+
+        public Toode(int id, string nimi, double price, bool aktiivne)
+        {
+            Id = id;
+            Nimi = nimi;
+            Price = price;
+            Aktiivne = aktiivne;
+        }
+    }
+
+}
