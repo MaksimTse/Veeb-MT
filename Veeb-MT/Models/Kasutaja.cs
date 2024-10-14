698e4721114440b1e2bafc02fdc8e6bdbf386c80
DarjaMiljukova <maksimplamja@gmail.com>
2024-10-14 20:58:58 +0300

    added class Kasutaja

---
+++ Veeb-MT/Models/Kasutaja.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Veeb_MT.Models
+{
+    public class Kasutaja
+    {
+        public int Id { get; set; }
+        public string Nimi { get; set; }
+        public int Parool { get; set; }
+        public string Eesnimi { get; set; }
+        public string Perenimi { get; set; }
+
+        public Kasutaja(int id, string nimi, int parool, string eesnimi, string perenimi)
+        {
+            Id = id;
+            Nimi = nimi;
+            Parool = parool;
+            Eesnimi = eesnimi;
+            Perenimi = perenimi;
+        }
+    }
+
+}
